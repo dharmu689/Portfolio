cc479bec09a0b0f6615d98bec19ba8562b406e28
Dharmu Kumar <dharmukumar1622003@gmail.com>
2026-06-05 19:02:32 +0530

    message backd

--- server/models/Message.js
+++ server/models/Message.js
@@ -4,14 +4,23 @@ const messageSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+    trim: true,
   },
   message: {
     type: String,
     required: true,
+    trim: true,
   },
   createdAt: {
     type: Date,
